3c16fa6f998d6a2dc4252decbf2998f2cea6d09b
Raja Singh <rajasinghqa832@gmail.com>
2020-05-18 11:23:53 +0530

    commited first time from eclipse to the local repository.

---
+++ SeleniumOne/src/testOne.java
@@ -0,0 +1,16 @@
+
+public class testOne {
+
+	public static void main(String[] args) {
+
+		String hungry = "Yes";
+		
+		if(hungry=="Yes"){
+			System.out.println("Please eat some food.");
+		}else{
+			System.out.println("Please do your work.");
+		}
+
+	}
+
+}
