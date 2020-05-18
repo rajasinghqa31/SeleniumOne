23a889606ec6bd1724c5a91c6e2896460e8e32d6
Raja Singh <rajasinghqa832@gmail.com>
2020-05-18 13:58:48 +0530

    testOne has been updated.

--- SeleniumOne/src/testOne.java
+++ SeleniumOne/src/testOne.java
@@ -5,7 +5,7 @@ public class testOne {
 
 		String hungry = "Yes";
 		
-		if(hungry=="Yes"){
+		if(hungry=="Yes" || hungry=="Y"){
 			System.out.println("Please eat some food.");
 		}else{
 			System.out.println("Please do your work.");
